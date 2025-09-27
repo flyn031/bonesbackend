72b0085b2792cce4c4a0906244dd3e9171731a5a
James oflynn <jamesoflynn@Jamess-Air.broadband>
2025-09-27 16:49:16 +0100

    Force complete TypeScript recompilation

--- dist/__tests__/setup.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Global test setup
-process.env.NODE_ENV = 'test';
-// Set up any other global test configurations here
--- dist/utils/prismaClient.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-// Create a single instance of Prisma client to use throughout the app
-const prisma = new client_1.PrismaClient();
-exports.default = prisma;
